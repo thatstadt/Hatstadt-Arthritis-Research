4a9ff0fd1c562e289c54ed410d5c8dbac88a44d5
Tyler Hatstadt <thatstadt@gmail.com>
2026-09-10 15:37:13 -0400

    Clarify additive risk model structure

--- src/SimulationEngine.java
+++ src/SimulationEngine.java
@@ -329,9 +329,6 @@ public class SimulationEngine {
             + bmiContribution;
 }
 
-        return risk;
-    }
-
     public static double sigmoid(double z) {
         return 1.0 / (1.0 + Math.exp(-z));
     }
